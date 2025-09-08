5982e1323b2b753e9d54edf0913b209bad587d1b
Minjae Kim <minjaekim9929@gmail.com>
2025-09-07 20:01:22 -0400

    feat: update Sidebar component to manage user selection and implement user search functionality

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,12 +1,25 @@
-import React, { useContext } from 'react';
-import assets, { userDummyData } from '../assets/assets';
+import React, { useContext, useEffect, useState } from 'react';
+import assets from '../assets/assets';
 import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../context/AuthContext';
+import { ChatContext } from '../../context/ChatContext';
 
-const Sidebar = ({ selectedUser, setSelectedUser }) => {
-	const { logout } = useContext(AuthContext);
+const Sidebar = () => {
+	const { getUsers, users, selectedUser, setSelectedUser, unseenMessages, setUnseenMessages } = useContext(ChatContext);
+
+	const { logout, onlineUsers } = useContext(AuthContext);
+
+	const [input, setInput] = useState(false);
 
 	const navigate = useNavigate();
+
+	const filteredUsers = input
+		? users.filter((user) => user.fullName.toLowerCase().includes(input.toLowerCase()))
+		: users;
+
+	useEffect(() => {
+		getUsers();
+	}, [onlineUsers]);
 
 	return (
 		<div
@@ -34,6 +47,7 @@ const Sidebar = ({ selectedUser, setSelectedUser }) => {
 				<div className="bg-[#282142] rounded-full flex items-center gap-2 py-3 px-4 mt-5">
 					<img src={assets.search_icon} alt="Search" className="w-3" />
 					<input
+						onChange={(e) => setInput(e.target.value)}
 						type="text"
 						className="bg-transparent border-none outline-none text-white text-xs placeholder-[#c8c8c8] flex-1"
 						placeholder="Search User..."
@@ -42,7 +56,7 @@ const Sidebar = ({ selectedUser, setSelectedUser }) => {
 			</div>
 
 			<div className="flex flex-col">
-				{userDummyData.map((user, index) => (
+				{filteredUsers.map((user, index) => (
 					<div
 						onClick={() => {
 							setSelectedUser(user);
@@ -55,15 +69,15 @@ const Sidebar = ({ selectedUser, setSelectedUser }) => {
 						<img src={user?.profilePic || assets.avatar_icon} alt="" className="w-[35px] aspect-[1/1] rounded-full" />
 						<div className="flex flex-col leading-5">
 							<p>{user.fullName}</p>
-							{index < 3 ? (
+							{onlineUsers.includes(user._id) ? (
 								<span className="text-green-400 text-xs">Online</span>
 							) : (
 								<span className="text-neutral-400 text-xs">Offline</span>
 							)}
 						</div>
-						{index > 2 && (
+						{unseenMessages[user._id] && (
 							<p className="absolute top-4 right-4 text-xs h-5 w-5 flex justify-center items-center rounded-full bg-violet-500/50">
-								{index}
+								{unseenMessages[user._id]}
 							</p>
 						)}
 					</div>
